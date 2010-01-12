cd2f75cd263eaf9fa891b730ab096ae7381a47a3
mattias <mattias@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-01-12 11:00:42 +0000

    Moblin + android packager plugins boilerplate
    git-svn-id: svn://localhost/public/Eclipse/trunk@475 6ad7a17e-d552-4a1e-ace4-f958fe14b117

---
+++ com.mobilesorcery.sdk.builder.android/src/com/mobilesorcery/sdk/builder/android/AndroidPackager.java
@@ -0,0 +1,18 @@
+package com.mobilesorcery.sdk.builder.android;
+
+import org.eclipse.core.runtime.CoreException;
+
+import com.mobilesorcery.sdk.core.AbstractPackager;
+import com.mobilesorcery.sdk.core.IBuildResult;
+import com.mobilesorcery.sdk.core.MoSyncProject;
+import com.mobilesorcery.sdk.profiles.IProfile;
+
+public class AndroidPackager extends AbstractPackager {
+
+	public void createPackage(MoSyncProject project, IProfile targetProfile,
+			IBuildResult buildResult) throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ com.mobilesorcery.sdk.builder.moblin/src/com/mobilesorcery/sdk/builder/moblin/MoblinPackager.java
@@ -0,0 +1,16 @@
+package com.mobilesorcery.sdk.builder.moblin;
+
+import org.eclipse.core.runtime.CoreException;
+
+import com.mobilesorcery.sdk.core.AbstractPackager;
+import com.mobilesorcery.sdk.core.IBuildResult;
+import com.mobilesorcery.sdk.core.MoSyncProject;
+import com.mobilesorcery.sdk.profiles.IProfile;
+
+public class MoblinPackager extends AbstractPackager {
+
+	public void createPackage(MoSyncProject project, IProfile targetProfile,
+			IBuildResult buildResult) throws CoreException {
+	}
+
+}
