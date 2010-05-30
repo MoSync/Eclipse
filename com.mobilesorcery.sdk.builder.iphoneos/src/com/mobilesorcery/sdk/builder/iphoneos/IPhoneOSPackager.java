20fd2757c13a122ca08b84d24b9db1f2c3c4c039
ali <ali@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-05-30 19:16:11 +0000

    Trunk: Eclipse, fixed iphone xcode template building. Icons remain.
    git-svn-id: svn://localhost/public/Eclipse/trunk@1328 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- com.mobilesorcery.sdk.builder.iphoneos/src/com/mobilesorcery/sdk/builder/iphoneos/IPhoneOSPackager.java
+++ com.mobilesorcery.sdk.builder.iphoneos/src/com/mobilesorcery/sdk/builder/iphoneos/IPhoneOSPackager.java
@@ -14,9 +14,11 @@
 package com.mobilesorcery.sdk.builder.iphoneos;
 
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.CoreException;
@@ -26,6 +28,8 @@ import com.mobilesorcery.sdk.core.IBuildVariant;
 import com.mobilesorcery.sdk.core.MoSyncProject;
 import com.mobilesorcery.sdk.core.AbstractPackager;
 import com.mobilesorcery.sdk.core.DefaultPackager;
+import com.mobilesorcery.sdk.core.MoSyncTool;
+import com.mobilesorcery.sdk.core.Util;
 import com.mobilesorcery.sdk.core.Version;
 
 
@@ -39,6 +43,13 @@ import com.mobilesorcery.sdk.core.Version;
 public class IPhoneOSPackager
 extends AbstractPackager
 {
+	private String m_iphoneBuildLoc;
+	
+	public IPhoneOSPackager ( ) 
+	{
+		MoSyncTool tool = MoSyncTool.getDefault( );
+		m_iphoneBuildLoc  = tool.getBinary( "iphone-builder" ).toOSString( );		
+	}
     
 
 
@@ -67,7 +78,7 @@ extends AbstractPackager
                                       variant );
 
         //
-        // Custom paramters
+        // Custom parameters
         //
         appName = intern.getParameters( ).get( DefaultPackager.APP_NAME );
         version = intern.getParameters( ).get( DefaultPackager.APP_VERSION );
@@ -76,18 +87,51 @@ extends AbstractPackager
         
         try
         {
-        	// Create XCode template
-			intern.runCommandLine( "%mosync-bin%\\iphone-builder",
-                                   "-project-name",
-                                   appName,
-                                   "-version",
-                                   new Version( version ).asCanonicalString( Version.MICRO ),
-                                   "-company-name",
-                                   company,
-					                "-input", 
-					                "%runtime-dir%\\template",
-					                "-output",
-					                "%package-output-dir%\\xcode-project" );    	
+        	String ver = new Version( version ).asCanonicalString( Version.MICRO );
+        	File   in  = intern.resolveFile( "%runtime-dir%/template" );
+        	File   out = intern.resolveFile( "%package-output-dir%/xcode-proj" );
+        	out.mkdirs( );
+        	
+        	// Create XCode template        	
+			int res = intern.runCommandLineWithRes( m_iphoneBuildLoc,
+                                                    "generate",
+                                                    "-project-name",
+                                                    appName,
+                                                    "-version",
+                                                    ver,
+                                                    "-company-name",
+                                                    company,
+					                                "-input", 
+					                                in.getAbsolutePath( ),
+					                                "-output",
+					                                out.getAbsolutePath( ) );
+			
+			// Did it run successfully?
+			if ( res != 0 )
+				return;
+			
+			// Copy program files to xcode template
+			Util.copyFile( new NullProgressMonitor( ), 
+				           intern.resolveFile( "%compile-output-dir%/data_section.bin" ), 
+				           new File( out, "data_section.bin" ) );
+			
+			Util.copyFile( new NullProgressMonitor( ), 
+			               intern.resolveFile( "%compile-output-dir%/rebuild.build.cpp" ), 
+			               new File( out, "Classes/rebuild.build.cpp" ) );
+			
+			File resources = intern.resolveFile( "%compile-output-dir%/resources" );
+			if ( resources.exists( ) == true )
+			{
+				Util.copyFile( new NullProgressMonitor( ), 
+			                   resources, 
+			                   new File( out, "resources" ) );
+			}
+			else
+			{
+				Util.writeToFile( new File( out, "resources" ), "" );
+			}
+			
+			buildResult.setBuildResult( out );
         }
         catch ( Exception e )
         {
