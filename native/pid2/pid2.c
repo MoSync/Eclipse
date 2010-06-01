e5c6bc92df90a74249fc46bd06cf954baabe8856
fmattias <fmattias@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-06-01 17:08:23 +0000

    trunk: Splitted .c-file into header and implementation.
    git-svn-id: svn://localhost/public/Eclipse/trunk@1355 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- native/pid2/pid2.c
+++ native/pid2/pid2.c
@@ -1,17 +1,31 @@
+/*  Copyright (C) 2009 Mobile Sorcery AB
+ 
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the Eclipse Public License v1.0.
+ 
+	This program is distributed in the hope that it will be useful, but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+	FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse Public License v1.0 for
+	more details.
+ 
+	You should have received a copy of the Eclipse Public License v1.0 along
+	with this program. It is also available at http://www.eclipse.org/legal/epl-v10.html
+*/
+
 #ifndef WIN32
 #include <unistd.h>
 #else
 #include <process.h>
 #endif
 
-int getpid2 ( void );
+#include "pid2.h"
 
 /**
  * Returns the pid of the proccess
  *
  * @return Proccess pid as int
  */
-int getpid2 ( void )
+PID2_API int pid(void)
 {
 #ifndef WIN32
 	return (int)getpid( );
