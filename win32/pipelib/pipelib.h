9147f2bbe7d1b82fe318ac5bb4e858ad415942dd
fmattias <fmattias@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-08-25 09:03:54 +0000

    trunk, eclipse: Fixed some problems occuring with pipelib in OS X. Since MoRE is started by a shell script the shell, when trying to kill it only the shell got killed. But now the kill signal is sent to all children.
    git-svn-id: svn://localhost/public/Eclipse/trunk@1840 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- win32/pipelib/pipelib.h
+++ win32/pipelib/pipelib.h
@@ -32,20 +32,95 @@
 #define PIPELIB_API PIPELIB_IMPORT
 #endif
 
+/**
+ * Creates a pipe.
+ *
+ * @param fds An array that will contain the read and write file descriptors
+ *            of the pipe upon return. It must be able to contain two ints,
+ *            the first element will be the read descriptor, and the second
+ *            the write descriptor.
+ * @return 0 on success, otherwise a negative number is returned.
+ */
 extern "C" PIPELIB_API int pipe_create(int* fds);
 
+/**
+ * Reads from a pipe with the given file descriptor.
+ *
+ * @param fd The file descriptor that specifies the read end of the pipe.
+ * @param dstbuf The buffer to read to.
+ * @param maxchars The number of bytes to read into the buffer.
+ * @return the number of bytes that was read, or -1 on error.
+ */
 extern "C" PIPELIB_API int pipe_read(int fd, void* dstbuf, int maxchars);
 
+/**
+ * Writes to a pipe with the given file descriptor.
+ *
+ * @param fd The file descriptor that specifies the write end of the pipe.
+ * @param srcbuf The buffer to write to.
+ * @param maxchars The number of bytes in the buffer to write.
+ * @return the number of bytes that was written, or -1 on error.
+ */
 extern "C" PIPELIB_API int pipe_write(int fd, void* srcbuf, int length);
 
+/**
+ * Duplicates the given file descriptor.
+ *
+ * @param fd The file descriptor to duplicate.
+ * @return The id of the file descriptor copy, or -1 on error.
+ */
 extern "C" PIPELIB_API int pipe_dup(int fd);
 
+/**
+ * Duplicates the given file descriptor such that the new file descriptor
+ * gets a given id.
+ *
+ * @param fd1 The file descriptor to duplicate.
+ * @param fd2 The id of the new file descriptor.
+ * @return The id of the file descriptor copy, or -1 on error.
+ */
 extern "C" PIPELIB_API int pipe_dup2(int fd1, int fd2);
 
+/**
+ * Closes the given file descriptor.
+ *
+ * @param fd File descriptor to close.
+ * @return 0 on success, -1 on error.
+ */
 extern "C" PIPELIB_API int pipe_close(int fd);
 
+/**
+ * Spawns a new process.
+ *
+ * Note: On *nix systems a new session is created such that the spawned
+ *       process is session and process group leader. This is will allow
+ *       us to send a signal to the whole process tree.
+ *
+ * @param cmd The path to the binary that will be executed.
+ * @param args The path to the binary followed by the arguments to it.
+ * @param dir Changes the current working directory to this before 
+ *            spawning the process.
+ * @return The pid of the spawned process, or negative on error.
+ *
+ */
 extern "C" PIPELIB_API int proc_spawn(char* cmd, char* args, char* dir);
 
-extern "C" PIPELIB_API int proc_wait_for(int handle);
+/**
+ * Waits for the given process to exit.
+ *
+ * @param pid Process id of the process to wait for.
+ * @return 0 if the process was waited for and terminated, -1 otherwise.
+ */
+extern "C" PIPELIB_API int proc_wait_for(int pid);
 
+/**
+ * Kills the given process.
+ *
+ * Note: On *nix all processes in the process group of the given process
+ *       get the kill signal.
+ *
+ * @param pid
+ * @param exit_code
+ * @return 0 on success, or negative on error.
+ */
 extern "C" PIPELIB_API int proc_kill(int pid, int exit_code);
