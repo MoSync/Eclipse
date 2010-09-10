1ab1a631bfc33bcd7c40ad72cbd034f37e07fe7d
mattias <mattias@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-09-10 14:14:38 +0000

    Forgotten file
    git-svn-id: svn://localhost/public/Eclipse/trunk@1937 6ad7a17e-d552-4a1e-ace4-f958fe14b117

---
+++ com.mobilesorcery.sdk.product/src/com/mobilesorcery/sdk/product/intro/actions/ExternalLinkAction.java
@@ -0,0 +1,30 @@
+package com.mobilesorcery.sdk.product.intro.actions;
+
+import java.net.URL;
+import java.util.Properties;
+
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.intro.IIntroSite;
+import org.eclipse.ui.intro.config.IIntroAction;
+
+import com.mobilesorcery.sdk.core.CoreMoSyncPlugin;
+import com.mobilesorcery.sdk.core.Util;
+import com.mobilesorcery.sdk.update.MosyncUpdatePlugin;
+
+public class ExternalLinkAction implements IIntroAction {
+
+    public void run(IIntroSite site, Properties params) {
+        String href = params.getProperty("href");
+        if (!Util.isEmpty(href)) {
+            try {
+                String url = Util.toGetUrl(href, MosyncUpdatePlugin.getDefaultParams(false));
+                IWebBrowser wb = PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser();
+                wb.openURL(new URL(url));
+            } catch (Exception e) {
+                CoreMoSyncPlugin.getDefault().log(e);
+            }
+        }
+    }
+
+}
