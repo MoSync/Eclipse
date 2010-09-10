d9e908bb792df06eaab1b367ece20baac34163a5
mattias <mattias@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-09-10 14:51:40 +0000

    Help actions can now show help in external browser
    git-svn-id: svn://localhost/public/Eclipse/trunk@1941 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- com.mobilesorcery.sdk.ui/src/com/mobilesorcery/ui/internal/actions/ShowHelpAction.java
+++ com.mobilesorcery.sdk.ui/src/com/mobilesorcery/ui/internal/actions/ShowHelpAction.java
@@ -1,10 +1,15 @@
 package com.mobilesorcery.ui.internal.actions;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
+import org.eclipse.cdt.utils.Platform;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -12,9 +17,13 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 
+import com.mobilesorcery.sdk.core.CoreMoSyncPlugin;
+import com.mobilesorcery.sdk.ui.MosyncUIPlugin;
+
 public class ShowHelpAction extends Action implements IWorkbenchWindowActionDelegate, IExecutableExtension {
 
     private String helpResource;
+    private Boolean showInExternalBrowser;
 
     public ShowHelpAction() {
         
@@ -23,6 +32,7 @@ public class ShowHelpAction extends Action implements IWorkbenchWindowActionDele
     public void setInitializationData(IConfigurationElement config, String propertyName, Object data) throws CoreException {
         if (data instanceof Map) {
             helpResource = (String) ((Map) data).get("resource");
+            showInExternalBrowser = Boolean.parseBoolean((String) ((Map) data).get("showInExternalBrowser"));
         }
     }
 
@@ -34,7 +44,18 @@ public class ShowHelpAction extends Action implements IWorkbenchWindowActionDele
 
     public void run(IAction action) {
         if (helpResource != null) {
-            PlatformUI.getWorkbench().getHelpSystem().displayHelpResource(helpResource);            
+            if (showInExternalBrowser) {
+                try {
+                    URL urlToHelpDoc = FileLocator.find(Platform.getBundle("com.mobilesorcery.sdk.help"), new Path(helpResource), null);
+                    URL fileUrlToHelpDoc = FileLocator.toFileURL(urlToHelpDoc);
+                    
+                    PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser().openURL(fileUrlToHelpDoc);
+                } catch (Exception e) {
+                    CoreMoSyncPlugin.getDefault().log(e);
+                }
+            } else {
+                PlatformUI.getWorkbench().getHelpSystem().displayHelpResource(helpResource);  
+            }
         }
     }
 
