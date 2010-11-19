fe512e4e0b39fca1fa2b81bc2c94873a69ea717f
micke <micke@6ad7a17e-d552-4a1e-ace4-f958fe14b117>
2010-11-19 14:46:12 +0000

    trunk, Eclipse, Android: Added portrait setting for screen orientation.
    git-svn-id: svn://localhost/public/Eclipse/trunk@2386 6ad7a17e-d552-4a1e-ace4-f958fe14b117

--- com.mobilesorcery.sdk.builder.android/src/com/mobilesorcery/sdk/builder/android/AndroidPackager.java
+++ com.mobilesorcery.sdk.builder.android/src/com/mobilesorcery/sdk/builder/android/AndroidPackager.java
@@ -399,7 +399,8 @@ extends AbstractPackager
 			+"\tandroid:versionName=\"" + version.toString() + "\">\n"
 			+"\t<application android:icon=\"@drawable/icon\" android:label=\"@string/app_name\">\n"
 				+"\t\t<activity android:name=\".MoSync\"\n"
-					// Don't use fixed orientation as default. +"\t\t\tandroid:screenOrientation=\"portrait\"\n"
+					// Use portrait orientation as default. 
+					+"\t\t\tandroid:screenOrientation=\"portrait\"\n"
 					+"\t\t\tandroid:configChanges=\"keyboardHidden|orientation\"\n"
 					+"\t\t\tandroid:label=\"@string/app_name\">\n"
 					+"\t\t\t<intent-filter>\n"
