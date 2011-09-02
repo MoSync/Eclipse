fb6142da919525c4cf16333c7e84e61a69b77c58
Mattias Bybro <mattias.bybro@mosync.com>
2011-09-02 11:52:12 +0200

    MOSYNCTWOSIX-204

--- com.mobilesorcery.sdk.builder.iphoneos/src/com/mobilesorcery/sdk/builder/iphoneos/IPhoneOSPackager.java
+++ com.mobilesorcery.sdk.builder.iphoneos/src/com/mobilesorcery/sdk/builder/iphoneos/IPhoneOSPackager.java
@@ -38,24 +38,24 @@ import com.mobilesorcery.sdk.core.Version;
 
 
 /**
- * Plugin entry point. Is responsible for creating xcode 
+ * Plugin entry point. Is responsible for creating xcode
  * project for iphone os devices.
- * 
+ *
  * @author Ali Mosavian
  */
 public class IPhoneOSPackager
 extends AbstractPackager
 {
 	public final static String ID = "com.mobilesorcery.sdk.build.ios.packager";
-	
-	private String m_iphoneBuildLoc;
-	
-	public IPhoneOSPackager ( ) 
+
+	private final String m_iphoneBuildLoc;
+
+	public IPhoneOSPackager ( )
 	{
 		MoSyncTool tool = MoSyncTool.getDefault( );
-		m_iphoneBuildLoc  = tool.getBinary( "iphone-builder" ).toOSString( );		
+		m_iphoneBuildLoc  = tool.getBinary( "iphone-builder" ).toOSString( );
 	}
-    
+
 
 
     /**
@@ -68,7 +68,8 @@ extends AbstractPackager
      *
      * @throws CoreException Error occurred
      */
-    public void createPackage ( MoSyncProject project,
+    @Override
+	public void createPackage ( MoSyncProject project,
                                 IBuildVariant variant,
                                 IBuildResult buildResult )
     throws CoreException
@@ -79,7 +80,7 @@ extends AbstractPackager
     	String cert;
     	IconManager     icon;
         DefaultPackager intern;
-        
+
 
         // Was used for printing to console
         intern = new DefaultPackager( project,
@@ -92,18 +93,18 @@ extends AbstractPackager
         version = intern.getParameters( ).get( DefaultPackager.APP_VERSION );
         company = intern.getParameters( ).get( DefaultPackager.APP_VENDOR_NAME );
         // We do not yet support configuration specific certs.
-        cert = PropertyUtil.getBoolean(project, PropertyInitializer.IPHONE_PROJECT_SPECIFIC_CERT) ? 
+        cert = PropertyUtil.getBoolean(project, PropertyInitializer.IPHONE_PROJECT_SPECIFIC_CERT) ?
         		project.getProperty(PropertyInitializer.IPHONE_CERT):
         		Activator.getDefault().getPreferenceStore().getString(PropertyInitializer.IPHONE_CERT);
-        
+
         try
         {
         	String ver = new Version( version ).asCanonicalString( Version.MICRO );
         	File   in  = intern.resolveFile( "%runtime-dir%/template" );
         	File   out = intern.resolveFile( "%package-output-dir%/xcode-proj" );
         	out.mkdirs( );
-        	
-        	// Create XCode template        	
+
+        	// Create XCode template
 			int res = intern.runCommandLineWithRes( m_iphoneBuildLoc,
                                                     "generate",
                                                     "-project-name",
@@ -114,66 +115,66 @@ extends AbstractPackager
                                                     company,
                                                     "-cert",
                                                     cert,
-					                                "-input", 
+					                                "-input",
 					                                in.getAbsolutePath( ),
 					                                "-output",
 					                                out.getAbsolutePath( ) );
-			
+
 			// Did it run successfully?
 			if ( res != 0 )
 				return;
-			
+
 			// Copy program files to xcode template
-			Util.copyFile( new NullProgressMonitor( ), 
-				           intern.resolveFile( "%compile-output-dir%/data_section.bin" ), 
+			Util.copyFile( new NullProgressMonitor( ),
+				           intern.resolveFile( "%compile-output-dir%/data_section.bin" ),
 				           new File( out, "data_section.bin" ) );
-			
-			Util.copyFile( new NullProgressMonitor( ), 
-			               intern.resolveFile( "%compile-output-dir%/rebuild.build.cpp" ), 
+
+			Util.copyFile( new NullProgressMonitor( ),
+			               intern.resolveFile( "%compile-output-dir%/rebuild.build.cpp" ),
 			               new File( out, "Classes/rebuild.build.cpp" ) );
-			
+
 			File resources = intern.resolveFile( "%compile-output-dir%/resources" );
 			if ( resources.exists( ) == true )
 			{
-				Util.copyFile( new NullProgressMonitor( ), 
-			                   resources, 
+				Util.copyFile( new NullProgressMonitor( ),
+			                   resources,
 			                   new File( out, "resources" ) );
 			}
 			else
 			{
 				Util.writeToFile( new File( out, "resources" ), "" );
 			}
-			
+
             //
             // Set icons here
             // Note: Always set a 48x48 png at the very least!
             //
             try
             {
-                File f;            	
+                File f;
 	            icon = new IconManager( intern,
-	            						project );            
-	            
-	            
+	            						project );
+
+
 	            // Set PNG icons
 	            if ( icon.hasIcon( "png" ) == true )
 	            {
 		            int[] sizes = {57, 72};
 		            for ( int s : sizes )
 		            {
-			            try 
-			            {	            
+			            try
+			            {
 			            	if ( s == 57 )
 			            		f = new File( out, "Icon.png" );
 			            	else
 			            		f = new File( out, "Icon-"+s+".png" );
-			            	
+
 			                if ( f.exists( ) == true )
 			                	f.delete( );
-			                
+
 			                icon.inject( f, s, s, "png" );
 			            }
-			            catch ( Exception e ) 
+			            catch ( Exception e )
 			            {
 			            	buildResult.addError( e.getMessage( ) );
 			            }
@@ -187,13 +188,13 @@ extends AbstractPackager
 	            } else {
 	            	intern.getConsole().addMessage("No XCode, will not build generated project");
 	            }
-	            
+
             }
-            catch ( Exception e ) 
+            catch ( Exception e )
             {
             	buildResult.addError( e.getMessage( ) );
-            }			
-			
+            }
+
 			buildResult.setBuildResult( out );
         }
         catch ( Exception e )
@@ -213,26 +214,25 @@ extends AbstractPackager
 		IBuildConfiguration cfg = project.getBuildConfiguration(cfgId);
 		boolean isDebugBuild = cfg != null && cfg.getTypes().contains(IBuildConfiguration.DEBUG_TYPE);
 		String target = isDebugBuild ? "Debug" : "Release";
-		String sdkId = getSDK(project, variant);
-		xcodeBuild.build(new Path(xcodeProject.getAbsolutePath()), target, sdkId);
-		
+		SDK sdk = getSDK(project, variant);
+		xcodeBuild.build(new Path(xcodeProject.getAbsolutePath()), target, sdk.getId());
+
+		String xcodeTarget = target + "-" + sdk.getSDKType();
 		// Hm, is this always true...?
-		String simSuffix = isSimulatorBuild(variant) ? "-iphonesimulator" : "";
-		return new Path(packager.resolve(xcodeProject.getAbsolutePath() + "/build/" + target + simSuffix + "/%project-name%.app"));
+		return new Path(packager.resolve(xcodeProject.getAbsolutePath() + "/build/" + xcodeTarget + "/%project-name%.app"));
 	}
 
 	private boolean isSimulatorBuild(IBuildVariant variant) {
 		return variant.getSpecifiers().containsKey(Activator.IOS_SIMULATOR_SPECIFIER);
 	}
 
-	private String getSDK(MoSyncProject project, IBuildVariant variant) throws CoreException {
+	private SDK getSDK(MoSyncProject project, IBuildVariant variant) throws CoreException {
 		String sdkId = null;
 		int sdkType = isSimulatorBuild(variant) ? XCodeBuild.IOS_SIMULATOR_SDKS : XCodeBuild.IOS_SDKS;
 		SDK sdk = Activator.getDefault().getSDK(project, sdkType);
 		if (sdk == null) {
 			throw new CoreException(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "No simulator SDK found, cannot build"));
 		}
-		sdkId = sdk.getId();
-		return sdkId;
+		return sdk;
 	}
 }
