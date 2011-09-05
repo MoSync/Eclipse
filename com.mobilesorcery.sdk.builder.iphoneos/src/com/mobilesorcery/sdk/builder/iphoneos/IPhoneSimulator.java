d0de1d7fca31abf846c1ccc3e5d7587782daca08
Mattias Bybro <mattias.bybro@mosync.com>
2011-09-05 23:36:21 +0200

    Added support for launching as iPad simulator

--- com.mobilesorcery.sdk.builder.iphoneos/src/com/mobilesorcery/sdk/builder/iphoneos/IPhoneSimulator.java
+++ com.mobilesorcery.sdk.builder.iphoneos/src/com/mobilesorcery/sdk/builder/iphoneos/IPhoneSimulator.java
@@ -14,24 +14,28 @@ public class IPhoneSimulator extends AbstractTool {
 	public static IPhoneSimulator getDefault() {
 		return new IPhoneSimulator(MoSyncTool.getDefault().getBinary("iphonesim"));
 	}
-	
+
 	protected IPhoneSimulator(IPath toolPath) {
 		super(toolPath);
 	}
-	
+
 	/**
 	 * Runs an iPhone app on the iPhone simulator
 	 * @param pathToApp The path to the app (the file ending with .app)
-	 * @param sdk The sdk to use, or <code>null</code> for the default
+	 * @param sdk The sdk to use, or {@code null }for the default
+	 * @param family The device family (eg iPhone, iPad) to simulate, or {@code null} for the default
 	 * @throws CoreException
 	 */
-	public void runApp(IPath pathToApp, String sdk) throws CoreException {
+	public void runApp(IPath pathToApp, String sdk, String family) throws CoreException {
 		ArrayList<String> cmd = new ArrayList<String>();
 		cmd.add(getToolPath().getAbsolutePath());
 		cmd.add("launch");
 		cmd.add(pathToApp.toOSString());
 		if (!Util.isEmpty(sdk)) {
 			cmd.add(sdk);
+		}
+		if (!Util.isEmpty(family)) {
+			cmd.add(family);
 		}
 		execute(cmd.toArray(new String[0]), null, null, false);
 	}
