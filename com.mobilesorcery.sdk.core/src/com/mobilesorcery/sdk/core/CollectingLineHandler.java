ebe25e872e91819416afd649bd398754b64edbd4
Mattias Bybro <mattias.bybro@mosync.com>
2011-10-11 07:51:46 +0200

    Jira down again: fixed potential race condition in process line handler

--- com.mobilesorcery.sdk.core/src/com/mobilesorcery/sdk/core/CollectingLineHandler.java
+++ com.mobilesorcery.sdk.core/src/com/mobilesorcery/sdk/core/CollectingLineHandler.java
@@ -2,6 +2,8 @@ package com.mobilesorcery.sdk.core;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import com.mobilesorcery.sdk.core.LineReader.LineAdapter;
@@ -9,37 +11,38 @@ import com.mobilesorcery.sdk.core.LineReader.LineAdapter;
 
 public class CollectingLineHandler extends LineAdapter {
 
-	ArrayList<String> lines = new ArrayList<String>();
+	List<String> lines = new ArrayList<String>();
 	private boolean stopped;
-	private int maxLines;
+	private final int maxLines;
 
 	public CollectingLineHandler() {
 		this(1024);
 	}
-	
+
 	public CollectingLineHandler(int maxLines) {
 		this.maxLines = maxLines;
 	}
-	
-	public void newLine(String line) {
+
+	@Override
+	public synchronized void newLine(String line) {
 		if (lines.size() <= maxLines) {
 			lines.add(line);
 		}
-		System.out.println(line);
 	}
 
+	@Override
 	public void stop(IOException e) {
 		stopped = true;
 	}
 
-	public List<String> getLines() {
-		return lines;
+	public synchronized List<String> getLines() {
+		return new ArrayList<String>(lines);
 	}
-	
-	public String getFirstLine() {
+
+	public synchronized String getFirstLine() {
 		return lines.size() == 0 ? "" : lines.get(0);
 	}
-	
+
 	public boolean isStopped() {
 		return stopped;
 	}
