697bd371509b931177b1e59eb5a6cc1c5b318b70
Mattias Bybro <mattias.bybro@mosync.com>
2012-02-06 13:10:19 +0100

    MOSYNC-1793: Fixed rebuild menu item

--- com.mobilesorcery.sdk.ui/src/com/mobilesorcery/sdk/ui/internal/handlers/BuildProjectHandler.java
+++ com.mobilesorcery.sdk.ui/src/com/mobilesorcery/sdk/ui/internal/handlers/BuildProjectHandler.java
@@ -13,6 +13,9 @@
  */
 package com.mobilesorcery.sdk.ui.internal.handlers;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
@@ -20,11 +23,13 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.BuildAction;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.mobilesorcery.sdk.core.CoreMoSyncPlugin;
 import com.mobilesorcery.sdk.ui.MoSyncCommandHandler;
+import com.mobilesorcery.sdk.ui.MosyncUIPlugin;
 
 /**
  * A command handler for building. This handler accepts the
@@ -33,10 +38,16 @@ import com.mobilesorcery.sdk.ui.MoSyncCommandHandler;
  * @author Mattias Bybro, mattias.bybro@purplescout.se
  *
  */
-public class BuildProjectHandler extends MoSyncCommandHandler {
+public class BuildProjectHandler extends MoSyncCommandHandler implements PropertyChangeListener {
 
     public BuildProjectHandler() {
+    	MosyncUIPlugin.getDefault().addListener(this);
+    	updateEnabled();
+    }
 
+    @Override
+	public void dispose() {
+    	MosyncUIPlugin.getDefault().removeListener(this);
     }
 
     @Override
@@ -61,17 +72,19 @@ public class BuildProjectHandler extends MoSyncCommandHandler {
         return null;
     }
 
-    @Override
-	public void setEnabled(Object context) {
-        setBaseEnabled(isEnabled());
+    private void updateEnabled() {
+    	boolean projectSelected = MosyncUIPlugin.getDefault().getCurrentlySelectedProject(PlatformUI.getWorkbench().getActiveWorkbenchWindow()) != null;
+        boolean autoBuild = ResourcesPlugin.getWorkspace().isAutoBuilding();
+    	boolean enabled = projectSelected && !autoBuild;
+        setBaseEnabled(enabled);
     }
 
-    @Override
-	public boolean isEnabled() {
-        // TODO: Support menu commands.
-        boolean enabled = !ResourcesPlugin.getWorkspace().isAutoBuilding();
-        return enabled;
-    }
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		if (event.getPropertyName() == MosyncUIPlugin.CURRENT_PROJECT_CHANGED) {
+			updateEnabled();
+		}
+	}
 
 
 
