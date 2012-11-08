b07f416e60edbd26f16bc2bdee03a3170e23ed76
Mattias Bybro <mattias.bybro@mosync.com>
2012-11-08 23:30:07 +0100

    MOSYNC-2147: Added missing import.

--- com.mobilesorcery.sdk.core/src/com/mobilesorcery/sdk/core/MoSyncBuilder.java
+++ com.mobilesorcery.sdk.core/src/com/mobilesorcery/sdk/core/MoSyncBuilder.java
@@ -21,6 +21,7 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
